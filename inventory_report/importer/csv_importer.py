f8f3722c8e7339e625be0b12896615db24f4be96
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2021-07-25 14:01:19 -0300

    req 3.

---
+++ inventory_report/importer/csv_importer.py
@@ -0,0 +1,20 @@
+import os
+from csv import DictReader
+from .importer import Importer
+
+
+class CsvImporter(Importer):
+    @classmethod
+    def import_data(cls, path):
+        _, file_extension = os.path.splitext(path)
+
+        if file_extension == ".csv":
+            return cls.open_file(path)
+        else:
+            raise ValueError("File must be a valid CSV format.")
+
+    @classmethod
+    def open_file(cls, path):
+        with open(path) as file:
+            data = DictReader(file)
+            return list(data)
---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,22 @@
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
+class Inventory:
+    @classmethod
+    def import_data(cls, filepath, report_type):
+        products = cls.import_products(filepath)
+        return cls.generate_report(products, report_type)
+
+    @classmethod
+    def import_products(cls, filepath):
+        if filepath.endswith(".csv"):
+            return CsvImporter.import_data(filepath)
+
+    @classmethod
+    def generate_report(cls, products_list, report_type):
+        if report_type == "simples":
+            return SimpleReport.generate(products_list)
+        else:
+            return CompleteReport.generate(products_list)
